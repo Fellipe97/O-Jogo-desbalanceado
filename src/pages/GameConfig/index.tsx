a8d4316a6ddc6d7fadeac6844f3963ba62738acf
Fellipe97 <lfalmeida97@hotmail.com>
2024-02-26 23:22:09 -0300

    feat: :sparkles: graficos dos personagens escolhidos

--- src/pages/GameConfig/index.tsx
+++ src/pages/GameConfig/index.tsx
@@ -1,21 +1,87 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-import { Container } from './styles';
+import {
+    Container,
+    TitleDiv,
+    Line,
+    CharacterDiv,
+    LeftCharacterDiv,
+    RightCharacterDiv,
+    NameCharacter,
+    CharacterBox,
+} from './styles';
 import { Button } from '../../components/Button';
+import { CharacterContext } from '../../context/CharacterContext';
+
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
+
 
 
 export const GameConfig = () => {
     const navigate = useNavigate();
+    const { character } = useContext(CharacterContext);
+
 
 
     return (
         <Container>
-            <h1>configuração</h1>
+            <TitleDiv>
+                <h1>Atributos dos Lutadores</h1>
+                <Line />
+            </TitleDiv>
+
+            <CharacterDiv>
+                {character && character?.map((item) => (
+                    <CharacterBox>
+                        <LeftCharacterDiv>
+                            <img src={item.pictureBig} />
+                        </LeftCharacterDiv>
+                        <RightCharacterDiv>
+                            <NameCharacter>{item.name}</NameCharacter>
+                            <PieChart
+                                series={[
+                                    {
+                                        data: [
+                                            { id: 0, value: item.strength, label: 'Força' },
+                                            { id: 1, value: item.agility, label: 'Agilidade' },
+                                            { id: 2, value: item.intelligence, label: 'Inteligência' },
+                                            { id: 3, value: item.resistance, label: 'Resistência' },
+                                        ],
+                                        highlightScope: { faded: 'global', highlighted: 'item' },
+                                        faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+
+                                    },
+                                ]}
+                                slotProps={{
+                                    legend: {
+                                        labelStyle: {
+                                            fontSize: 14,
+                                            fontWeight: 'bold',
+                                            fill: '#ffffff',
+                                        },
+                                    },
+                                }}
+                                width={400}
+                                height={250}
+                            />
+                        </RightCharacterDiv>
+                    </CharacterBox>
+                ))}
+            </CharacterDiv>
+
+            <TitleDiv style={{ marginTop: 20 }}>
+                <h1>Configuração</h1>
+                <Line />
+            </TitleDiv>
+            
+            <h1>!!!!!!!!!! FALTA A CONFIGURALÇÃO DO JOGO !!!!!!!!!!!!!!!!!!!</h1>
             <Button
                 title='Voltar'
+                color='redButton'
                 onClick={() => navigate('/selectionFighters')}
             />
+
         </Container>
     );
 };
--- src/pages/GameConfig/styles.ts
+++ src/pages/GameConfig/styles.ts
@@ -4,9 +4,69 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-
     height: 100vh;
 
+    padding-inline: 10%;
+    padding-block: 20px;
+`;
+
+export const TitleDiv = styled.div`
+
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    padding: 10px;
+
+    h1{
+        color: #A283D3;
+        font-size: 40px;
+        font-weight: bold;
+    }
+`;
+
+export const Line = styled.div`
+    width: 100%;
+    height: 5px;
+
+    background-color: #8C61B3;
+
+    border-radius: 100px;
+`;
+
+
+export const CharacterDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    gap: 10px;
+`;
+
+export const CharacterBox = styled.div`
+    display: flex;
+    gap: 20px;
+`;
+
+export const LeftCharacterDiv = styled.div`
+    //width: 50%;
+    display: flex;
+
+    img{
+        width: 250px;
+        border-radius: 10px;
+        object-fit: cover;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+`;
+
+export const RightCharacterDiv = styled.div`
+    flex:1;
+    padding: 20px;
+`;
+
+export const NameCharacter = styled.h1`
+    flex:1;
+    color: #ffffff;
 `;
